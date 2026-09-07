2a33b7916088e9fae14817822b4da238ed875a56
Nazar Yestayev <legion2440@mail.ru>
2026-09-07 23:31:38 +0500

    chore: keep HTTP test helpers warning-free

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 mod cgi;
 mod config;
+// `http` also exposes a whole-request convenience parser used by unit tests;
+// the server itself uses the incremental head/body API.
+#[allow(dead_code)]
 mod http;
 mod server;
 mod util;
